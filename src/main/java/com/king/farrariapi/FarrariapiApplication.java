324095dddf824f1d3b809fe45938b2c04e108ecc
Chris King <cking5836@gmail.com>
2021-04-25 19:38:08 -0400

    Ran spring boot init

---
+++ src/main/java/com/king/farrariapi/FarrariapiApplication.java
@@ -0,0 +1,13 @@
+package com.king.farrariapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarrariapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarrariapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/king/farrariapi/FarrariapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.king.farrariapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarrariapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
